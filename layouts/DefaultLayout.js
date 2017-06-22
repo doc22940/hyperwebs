99802a1c4f9a8945070ea466889b1ddb7c3889e9
Cole Peters <cole@colepeters.com>
2017-06-22 19:53:26 +0100

    🚀 Initial commit woooooo pew pew

---
+++ layouts/DefaultLayout.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Head from 'next/head'
+import { rehydrate } from 'glamor'
+import PropTypes from 'prop-types'
+
+// Adds server generated styles to glamor cache.
+// Has to run before any `style()` calls
+// '__NEXT_DATA__.ids' is set in '_document.js'
+if (typeof window !== 'undefined') {
+  rehydrate(window.__NEXT_DATA__.ids)
+}
+
+const DefaultLayout = ({ title, children, ...props }) => (
+  <div {...props}>
+    <Head>
+      <title>{ `Cole Peters, designer/developer` }</title>
+    </Head>
+
+    { children }
+  </div>
+)
+DefaultLayout.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired
+}
+
+export default DefaultLayout
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Heading } from 'axs'
+
+import DefaultLayout from '../layouts/DefaultLayout'
+
+export default () => (
+  <DefaultLayout title='Home'>
+    <Heading>
+      Hello next
+    </Heading>
+  </DefaultLayout>
+)

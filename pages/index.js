c9980742caca7a873bbcfaa3a2e76657f69cf775
Cole Peters <cole@colepeters.com>
2017-07-03 19:14:33 +0100

    WIP

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-import { Box, Text } from 'axs'
+import { Box, Text, Heading } from 'axs'
 
 import DefaultLayout from '../layouts/DefaultLayout'
 import TopBar from '../components/TopBar'
@@ -15,6 +15,21 @@ export default () => (
       <Text is='p' width={[1, 3 / 4, 2 / 3]}>
         I operate a remote freelance design practice out of London, England (in addition to being <a href='https://colepeters.com' style={{ fontWeight: 400 }}>a&nbsp;photographer</a>). Interested in working together? <Link to='/hire'>Get&nbsp;in&nbsp;touch</Link>.
       </Text>
+    </Box>
+    <Box is='section' px={[3, 4]}>
+      <Heading fontSize={1} mb={[2, 3]}>
+        Selected work
+      </Heading>
+      <Box p={[2, 3]} width={[1, 1 / 2, 1 / 3]} css={{ boxShadow: '0 1px 4px rgba(0,0,0,0.05), 0 12px 24px rgba(0,0,0,0.1)' }}>
+        <Text is='p'>
+          Hi
+        </Text>
+      </Box>
+      <Box p={[2, 3]} width={[1, 1 / 2, 1 / 3]} css={{ boxShadow: '0 1px 4px rgba(0,0,0,0.05), 0 12px 24px rgba(0,0,0,0.1)' }}>
+        <Text is='p'>
+          Hi
+        </Text>
+      </Box>
     </Box>
   </DefaultLayout>
 )
